c5d8f6e9170ff1c8984a0dc34618fde7cadf1f9a
Sam Yao <yaosam@yeah.net>
2025-01-21 22:09:12 +0800

    Update add_feed.sh

--- scripts/add_feed.sh
+++ scripts/add_feed.sh
@@ -1,8 +1,9 @@
 # 进入openwrt目录
 # cd ~/openwrt
 
-# 添加自定义源
-# cat >> feeds.conf.default <<EOF
-# src-git kiddin9 https://github.com/kiddin9/kwrt-packages
-# EOF
-git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+添加自定义源
+cat >> feeds.conf.default <<EOF
+src-git tailscale https://github.com/asvow/luci-app-tailscale
+EOF
+# git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+./scripts/feeds update -a && ./scripts/feeds install -a
